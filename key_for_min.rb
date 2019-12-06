45450fb97e30157448511bc2a68be32cdcae5654
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-06 15:40:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,15 +6,28 @@
 # hash1 = {:blake => 500, :ashley => 2, :adam => 1}
 # hash2 = {:blake => 10, :ashley => 50, :adam => 17}
 def key_for_min_value(name_hash)
-  name_hash.collect  do |key, value|
     if name_hash == {}
        return nil
     else
-       
-       return key
+       name_hash = name_hash.sort_by {|key, value| value}
+       name_hash[0]
     end
   end
 end
+
+
+
+# def key_for_min_value(name_hash)
+#   name_hash.collect  do |key, value|
+#     if name_hash == {}
+#        return nil
+#     else
+#        name_hash = name_hash.sort_by 
+#        return key
+#     end
+#   end
+# end
+
 
 
 # birthday_kids.collect do |name, age|
