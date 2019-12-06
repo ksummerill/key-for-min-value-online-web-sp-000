226afe9f06ad785b0cc9178bc2974aa274c3b578
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-06 15:17:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+# This method should iterate over the hash and return the key (not the value!) 
+# that points to the smallest value of the set
+# hash = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
-
+  key_for_min_value.sort  do |key, value|
+    key <=> value
 end
+
+
+# array = [7, 3, 1, 2, 6, 5]
+#  
+# array.sort do |a, b|
+#   a <=> b
+# end
