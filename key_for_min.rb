5243afc47dd0cb43522a9903f66c92ba6ea54ea2
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-06 15:50:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,24 +3,12 @@
 
 # This method should iterate over the hash and return the key (not the value!)
 # that points to the smallest value of the set
-# hash1 = {:blake => 500, :ashley => 2, :adam => 1}
-# hash2 = {:blake => 10, :ashley => 50, :adam => 17}
-def key_for_min_value(name_hash)
-    if name_hash == {}
-       return nil
-    else
-       name_hash = name_hash.collect {|key, value| value}
-       name_hash[0][0]
-    end
-end
-
-
 
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = 1000000
-  hash.each do |k, v|
-    if v < min_value
+  hash.each do |key, value|
+    if value < min_value
       min_value = value
       min_key = key
     end
